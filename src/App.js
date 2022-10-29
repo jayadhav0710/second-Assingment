12a146f4d2d99bc3a52f01792d7cb3614ca8a6cf
jayadhav0710 <110915141+jayadhav0710@users.noreply.github.com>
2022-10-29 16:03:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import logo from './assets/arbnb-logo.jpg'
+
+function App() {
+  return (
+    <div className='App'>
+     <div className='head'>
+     <h1 >The Evolution of </h1>
+     <h1 >Airbnb's Frontend</h1>
+     </div>
+     <p>@spikebrehm</p>
+     <img src={logo}></img>
+
+
+    </div>
+  );
+}
+
+export default App;
